2847bc2f5b84312b2b788c8ac68bc9848a38ad2a
GitHub Action <action@github.com>
2022-08-15 20:11:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/michalszymanski/poimandres/services/MyProjectService.kt
+++ src/main/kotlin/com/github/michalszymanski/poimandres/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.michalszymanski.poimandres.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.michalszymanski.poimandres.MyBundle
 
 class MyProjectService(project: Project) {
 
